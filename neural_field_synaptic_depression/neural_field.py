a1a20fe6036e8c2cc9f483529ca3aab0fde58e73
Sage Shaw <sage.b.shaw@gmail.com>
2024-01-22 12:34:40 -0700

    2D sim

--- neural_field_synaptic_depression/neural_field.py
+++ neural_field_synaptic_depression/neural_field.py
@@ -1,7 +1,7 @@
-'''
+"""
 A class that solves the neural field equation  with synaptic depression, using
 method of lines.
-'''
+"""
 
 from dataclasses import dataclass
 from math import ceil
@@ -12,43 +12,42 @@ from typing import Callable
 
 
 class Parameters:
-
     def __init__(self, *, mu: float, alpha: float, gamma: float):
         self.mu = mu
         self.alpha = alpha
         self.gamma = gamma
-        self.beta = 1/gamma - 1
+        self.beta = 1 / gamma - 1
 
     @property
     def dict(self):
         return {
-            'mu': self.mu,
-            'alpha': self.alpha,
-            'beta': self.beta,
-            'gamma': self.gamma}
+            "mu": self.mu,
+            "alpha": self.alpha,
+            "beta": self.beta,
+            "gamma": self.gamma,
+        }
 
     def __repr__(self):
-        return 'Params(' + str(self.dict) + ')'
+        return "Params(" + str(self.dict) + ")"
 
 
 class ParametersBeta(Parameters):
-
     def __init__(self, *, mu: float, alpha: float, beta: float):
         self.mu = mu
         self.alpha = alpha
         self.beta = beta
 
-        self.gamma = 1/(1 + beta)
+        self.gamma = 1 / (1 + beta)
 
 
 class NeuralField:
-
-    def __init__(self,
-                 space: SpaceDomain,
-                 firing_rate: Callable[[np.ndarray], np.ndarray],
-                 weight_kernel: Callable[[np.ndarray], np.ndarray],
-                 params: Parameters):
-
+    def __init__(
+        self,
+        space: SpaceDomain,
+        firing_rate: Callable[[np.ndarray], np.ndarray],
+        weight_kernel: Callable[[np.ndarray], np.ndarray],
+        params: Parameters,
+    ):
         self.space = space
         self.firing_rate = firing_rate
         self.weight_kernel = weight_kernel
@@ -58,45 +57,49 @@ class NeuralField:
 
     def initialize_convolution(self):
         h = self.space.spacing
-        kernel_half_width = ceil(16/h*np.log(10))
-        kernel_xs = h*np.arange(-kernel_half_width, kernel_half_width+1)
-        self.kernel = h*self.weight_kernel(kernel_xs)
+        kernel_half_width = ceil(16 / h * np.log(10))
+        kernel_xs = h * np.arange(-kernel_half_width, kernel_half_width + 1)
+        self.kernel = h * self.weight_kernel(kernel_xs)
 
     def conv(self, y: np.ndarray):
-        return fftconvolve(y, self.kernel, mode='same')
+        return fftconvolve(y, self.kernel, mode="same")
 
     def rhs(self, t, v):
         u, q = v
         temp = self.firing_rate(u)
         v_new = np.empty_like(v)
-        v_new[0] = 1/self.params.mu * (-u + self.conv(q*temp))
-        v_new[1] = (1 - q - self.params.beta*q*temp)/self.params.alpha
+        v_new[0] = 1 / self.params.mu * (-u + self.conv(q * temp))
+        v_new[1] = (1 - q - self.params.beta * q * temp) / self.params.alpha
         return v_new
 
 
 class NeuralFieldMatrixConvolv(NeuralField):
-
     def initialize_convolution(self):
         # A matrix of quatrature weights
         # Assuming the values at the boundary are zero, this is
         # essentially a trapezoidal rule.
-        self.M = self.space.spacing*self.weight_kernel(np.subtract.outer(
-                     self.space.array,
-                     self.space.array))
+        self.M = self.space.spacing * self.weight_kernel(
+            np.subtract.outer(self.space.array, self.space.array)
+        )
 
     def conv(self, y: np.ndarray):
-        return self.M@y
+        return self.M @ y
+
 
 # sample firing rate functions
 
+
 def heaviside_firing_rate(y: np.ndarray, theta: float) -> np.ndarray:
-    return np.heaviside(y-theta, .5)
+    return np.heaviside(y - theta, 0.5)
+
 
 # weight kernel functions
 
+
 def exponential_weight_kernel(y: np.ndarray) -> np.ndarray:
-    return .5*np.exp(-np.abs(y))
+    return 0.5 * np.exp(-np.abs(y))
+
 
 def wizzard_hat(y: np.ndarray) -> np.ndarray:
     yabs = np.abs(y)
-    return (1 - yabs)*np.exp(-yabs)
+    return (1 - yabs) * np.exp(-yabs)
--- torus_experiments/anim_test.py
+++ torus_experiments/anim_test.py
@@ -24,19 +24,19 @@ from neural_field_synaptic_depression.time_integrator_tqdm import TqdmWrapper
 
 FILE_NAME = os.path.join(experiment_defaults.media_path, "generation.gif")
 
-params = Parameters(mu=1.0, alpha=20.0, gamma=1)
-
-linspace_params = (-100, 100, 201)
+params = Parameters(mu=1.0, alpha=50.0, gamma=.1)
+linspace_params = (-100, 100, 401)
+time = TimeDomain_Start_Stop_MaxSpacing(0, 30, 1e-2)
 
 space = SpaceDomain2D(*linspace_params, *linspace_params)
-time = TimeDomain_Start_Stop_MaxSpacing(0, 30, 1e-2)
 
 u0 = np.ones((2, *space.X.shape))
 u0[0] = space.dist(0, 0) < 10
 
 
-def firing_rate(u):
-    return u > 0.2
+def firing_rate(u, theta=0.2):
+    # return u > 0.2
+    return 1 / (1 + np.exp(-20 * (u - theta)))
 
 
 def weight_kernel(r):
@@ -55,6 +55,14 @@ solver = TqdmWrapper(Euler())
 plt.figure("Wave")
 color_mesh = plt.pcolormesh(space.X, space.Y, u0[0], cmap="gnuplot")
 plt.colorbar(color_mesh)
-for u in islice(solver.solution_generator(u0, model.rhs, time), None, None, 10):
-    color_mesh.set_array(u[0].ravel())
-    plt.pause(1e-3)
+
+with imageio.get_writer(FILE_NAME, mode="I") as writer:
+    for u in islice(solver.solution_generator(u0, model.rhs, time), None, None, 10):
+        color_mesh.set_array(u[0].ravel())
+        plt.savefig(FILE_NAME + ".png")
+        image = imageio.imread(FILE_NAME + ".png")
+        writer.append_data(image)
+        plt.pause(1e-3)
+
+os.remove(FILE_NAME + ".png")
+plt.close()
